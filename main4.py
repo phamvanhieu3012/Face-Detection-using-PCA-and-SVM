608da68d6702897f6763a7fef889a4e85b55a6e1
phamvanhieu3012 <phamvanhieu74123@gmail.com>
2021-11-14 21:29:30 +0700

    Update main3

--- main4.py
+++ main4.py
@@ -7,9 +7,10 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 import cv2
+import pickle
 
 #Path to the root image directory containing sub-directories of images
-path="105_classes_pins_dataset/"
+path="dataset/"
 
 data_slice = [70,195,78,172] # [ ymin, ymax, xmin, xmax]
 resize_ratio = 2.5
@@ -17,40 +18,61 @@ h = int((data_slice[1] - data_slice[0])/resize_ratio) #ymax - ymin slice, Height
 w = int((data_slice[3] - data_slice[2])/resize_ratio) #xmax - xmin slice, Width of image in float
 print("Image dimension after resize (h,w) :", h, w)
 
-n_sample = 0 #Initial sample count
-label_count = 0 #Initial label count
-n_classes = 0 #Initial class count
+# n_sample = 0 #Initial sample count
+# label_count = 0 #Initial label count
+# n_classes = 0 #Initial class count
 
 #PCA Component
 n_components = 7
 
-##2
+##2 D
 
 #Flat image Feature Vector
-X=[]
-#Int array of Label Vector
-Y=[]
+# X=[]
+# #Int array of Label Vector
+# Y=[]
+#
+# target_names = [] #Array to store the names of the persons
+#
+# for directory in os.listdir(path):
+#     for file in os.listdir(path+directory):
+#         img=cv2.imread(path+directory+"/"+file)
+#         img=cv2.resize(img, (w,h))
+#         img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#         featurevector=numpy.array(img).flatten()
+#         X.append(featurevector)
+#         Y.append(label_count)
+#         n_sample = n_sample + 1
+#     target_names.append(directory)
+#     label_count=label_count+1
+#
+# print("Samples :", n_sample)
+# print("Class :", target_names)
+# n_classes = len(target_names)
+#
+# # Ghi data
+# pick_in = open('models/dataX.pickle','wb') #Write file
+# pickle.dump(X,pick_in)
+# pick_in.close()
+#
+# with open('models/dataY.pkl', 'wb') as f: #Write file
+#     pickle.dump(Y, f)
+#
+# with open('models/dataNames.pkl', 'wb') as f:
+#     pickle.dump(target_names, f)
 
-target_names = [] #Array to store the names of the persons
+# Đọc data
+pick_in = open('models/dataX.pickle', 'rb')  # Read file
+X = pickle.load(pick_in)
+pick_in.close()
 
-h=128
-w=70
+with open('models/dataY.pkl', 'rb') as f:
+    Y = pickle.load(f)
 
-for directory in os.listdir(path):
-    for file in os.listdir(path+directory):
-        img=cv2.imread(path+directory+"/"+file, 0)
-        img=cv2.resize(img, (h,w))
-        # img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        featurevector=numpy.array(img).flatten()
-        X.append(featurevector)
-        Y.append(label_count)
-        n_sample = n_sample + 1
-    target_names.append(directory)
-    label_count=label_count+1
+with open('models/dataNames.pkl', 'rb') as f:
+    target_names = pickle.load(f)
 
-print("Samples :", n_sample)
-print("Class :", target_names)
-n_classes = len(target_names)
+# print(X)
 
 ##3 PCA
 
@@ -82,75 +104,83 @@ X_test_pca = pca.transform(X_test)
 print("done in %0.3fs" % (time() - t0))
 
 ##4 Show 7 ảnh sau PCA
-import matplotlib.pyplot as plt
-
-def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
-    """Helper function to plot a gallery of portraits"""
-    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
-    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
-    for i in range(n_components):
-        plt.subplot(n_row, n_col, i + 1)
-        plt.imshow(images[i], cmap=plt.cm.gray)
-        plt.title(titles[i], size=12)
-        plt.xticks(())
-    plt.show()
-
-
-eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
-plot_gallery(eigenfaces, eigenface_titles, h, w)
+# import matplotlib.pyplot as plt
+#
+# def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
+#     """Helper function to plot a gallery of portraits"""
+#     plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+#     plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+#     for i in range(n_components):
+#         plt.subplot(n_row, n_col, i + 1)
+#         plt.imshow(images[i], cmap=plt.cm.gray)
+#         plt.title(titles[i], size=12)
+#         plt.xticks(())
+#     plt.show()
+#
+#
+# eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
+# plot_gallery(eigenfaces, eigenface_titles, h, w)
 
 ##5
 
 ###############################################################################
 # Train a SVM classification model
-print("\n")
-print("Fitting the classifier to the training set")
-t0 = time()
-param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
-              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
-clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid, verbose=5)
-clf = clf.fit(X_train_pca, y_train)
-print("done in %0.3fs" % (time() - t0))
+# print("\n")
+# print("Fitting the classifier to the training set")
+# t0 = time()
+# param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+#               'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
+# clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid, verbose=5)
+# clf = clf.fit(X_train_pca, y_train)
+# print("done in %0.3fs" % (time() - t0))
+#
+# print("\n")
+# print("Best estimator found by grid search : ")
+# print(clf.best_estimator_)
+#
+# # Ghi model
+# pick = open('models/model.sav','wb')
+# pickle.dump(clf,pick)
+# pick.close()
 
-print("\n")
-print("Best estimator found by grid search : ")
-print(clf.best_estimator_)
+# ##6 Test
 
-###############################################################################
-# Quantitative evaluation of the model quality on the test set
-print("\n")
-print("Predicting people's names on the test set")
-t0 = time()
-y_pred = clf.predict(X_test_pca)
-print(clf.score(X_test_pca,y_test))
-print("done in %0.3fs" % (time() - t0))
+# Đọc model
+pick = open('models/model.sav', 'rb')
+clf = pickle.load(pick)
+pick.close()
 
-print("\nClassification Report : ")
-print(classification_report(y_test, y_pred, target_names=target_names))
-
-print("\nConfusion Matrix : ")
-print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
-
-##6 Test
 
 ###############################################################################
 # Prediction of user based on the model
 
-test = []
-#testImage = "test/Tiger_Woods_0010.jpg"
-testImage = "anhtest.jpg"
+# test = []
+# #testImage = "test/Tiger_Woods_0010.jpg"
+# testImage = "test/anhtest.jpg"
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#
+# testImage=cv2.imread(testImage)[data_slice[0]:data_slice[1], data_slice[2]:data_slice[3]]
+# testImage=cv2.resize(testImage, (w,h))
+# testImage=cv2.cvtColor(testImage, cv2.COLOR_BGR2GRAY)
+# testImageFeatureVector=numpy.array(testImage).flatten()
+# test.append(testImageFeatureVector)
+# testImagePCA = pca.transform(test)
+# testImagePredict=clf.predict(testImagePCA)
+#
+# print ("Predicted Name : " + target_names[testImagePredict[0]])
 
+## Xuat anh
 
-testImage=cv2.imread(testImage)[data_slice[0]:data_slice[1], data_slice[2]:data_slice[3]]
-testImage=cv2.resize(testImage, (w,h))
-testImage=cv2.cvtColor(testImage, cv2.COLOR_BGR2GRAY)
-testImageFeatureVector=numpy.array(testImage).flatten()
-test.append(testImageFeatureVector)
-testImagePCA = pca.transform(test)
-testImagePredict=clf.predict(testImagePCA)
-
-
-print ("Predicted Name : " + target_names[testImagePredict[0]])
+# testImage3 = cv2.imread("test/anhtest.jpg")
+# testImage2 =cv2.resize(testImage3, (200, 250))
+# gray = cv2.cvtColor(testImage2, cv2.COLOR_BGR2GRAY)
+# faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+# for (x, y, w, h) in faces:
+#     cv2.rectangle(testImage2, (x, y), (x + w, y + h), (255, 0, 0), 3)
+#     cv2.putText(testImage2, target_names[testImagePredict[0]], (x + x // 10, y + h + 20), \
+#                                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+# cv2.imshow('img', testImage2)
+# cv2.waitKey()
 
 ##8 Mở cam test
 
